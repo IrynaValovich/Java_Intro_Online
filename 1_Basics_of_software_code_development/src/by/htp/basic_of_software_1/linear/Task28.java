fd29e17d4486e48e4f7351caf4fdb2c001574da6
Iryna Valovich <83275959+IrynaValovich@users.noreply.github.com>
2022-01-26 16:47:38 +0300

    Update Task28.java

--- 1_Basics_of_software_code_development/src/by/htp/basic_of_software_1/linear/Task28.java
+++ 1_Basics_of_software_code_development/src/by/htp/basic_of_software_1/linear/Task28.java
@@ -7,11 +7,11 @@ public class Task28 {
 	public static void main(String[] args) {		
 		double iRad = 1;		
 		double iDegr;        
-        iDegr = iRad * 180 / Math.PI;        
-        int degree = (int) iDegr;        
-        double temp = (iDegr - degree) * 60;         
-        int minute = (int) temp;        
-        int second = (int) ((temp - minute) * 60);        
-        System.out.println(String.format("%s%s° %s%s' %s%s''", degree / 10, degree % 10, minute / 10, minute % 10, second / 10, second % 10));        
+		iDegr = iRad * 180 / Math.PI;        
+		int degree = (int) iDegr;        
+		double temp = (iDegr - degree) * 60;         
+		int minute = (int) temp;        
+		int second = (int) ((temp - minute) * 60);        
+		System.out.println(String.format("%s%s° %s%s' %s%s''", degree / 10, degree % 10, minute / 10, minute % 10, second / 10, second % 10));        
 	}	
 }
