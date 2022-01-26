254fc616fd3750fb76e65c9f20430fa174240b60
Iryna Valovich <83275959+IrynaValovich@users.noreply.github.com>
2022-01-26 16:49:10 +0300

    Update Task30.java

--- 1_Basics_of_software_code_development/src/by/htp/basic_of_software_1/linear/Task30.java
+++ 1_Basics_of_software_code_development/src/by/htp/basic_of_software_1/linear/Task30.java
@@ -8,9 +8,9 @@ public class Task30 {
 		double r1 = 200;
 		double r2 = 470; 
 		double r3 = 220;
-        double r;        
-        r = 1 / (1 / r1 + 1 / r2 + 1 / r3);
-        System.out.println("При R1 = " + r1 + ", R2 = " + r2 + " и R3 = " + r3);        
-        System.out.println("Сопротивление соединения = " + r);
+		double r;        
+		r = 1 / (1 / r1 + 1 / r2 + 1 / r3);
+		System.out.println("При R1 = " + r1 + ", R2 = " + r2 + " и R3 = " + r3);        
+		System.out.println("Сопротивление соединения = " + r);
 	}
 }
