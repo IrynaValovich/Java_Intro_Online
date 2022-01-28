578856db18e2609d9866e5c6fdbb4153a2a2cf14
Iryna Valovich <83275959+IrynaValovich@users.noreply.github.com>
2022-01-28 17:40:09 +0300

    Update Task16.java

--- 2_Algorithmization/src/by/htp/algorithmization_2/multidim_array/Task16.java
+++ 2_Algorithmization/src/by/htp/algorithmization_2/multidim_array/Task16.java
@@ -19,32 +19,32 @@ public class Task16 {
 	}	
 	
 	public static void magicSquare(int[][] matrix, int n) {		
-        int i = 0;
-        int j = n / 2;
-        for (int current = 1; current <= n * n; current++) {
-            matrix[i][j] = current;
-            if (current % n == 0) { // если текущее число кратно n, спуск вниз
-                i++;
-            } else {// иначе подъем и влево
-                i--;
-                j++;
-                if (i < 0) {
-                	i = n - 1;
-                }                
-                if (j > n-1) {
-                	j = 0;
-                }
-            }
-        }
-    }	
+		int i = 0;
+		int j = n / 2;
+		for (int current = 1; current <= n * n; current++) {
+		    matrix[i][j] = current;
+		    if (current % n == 0) { // если текущее число кратно n, спуск вниз
+			i++;
+		    } else {// иначе подъем и влево
+			i--;
+			j++;
+			if (i < 0) {
+				i = n - 1;
+			}                
+			if (j > n-1) {
+				j = 0;
+			}
+		    }
+		}
+	}	
 	
 	public static void printMagicSquare(int[][] matrix) {		
 		System.out.println("Магический квадрат: ");		
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+		for (int i = 0; i < matrix.length; i++) {
+		    for (int j = 0; j < matrix[i].length; j++) {
+			System.out.print(matrix[i][j] + " ");
+		    }
+		    System.out.println();
+		}
     }	
 }
