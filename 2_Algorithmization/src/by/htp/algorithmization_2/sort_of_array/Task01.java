0af936ed4090cedeb12d7c5e3111bd1a2e1b7c45
Iryna Valovich <83275959+IrynaValovich@users.noreply.github.com>
2022-01-30 16:15:37 +0300

    Update Task01.java

--- 2_Algorithmization/src/by/htp/algorithmization_2/sort_of_array/Task01.java
+++ 2_Algorithmization/src/by/htp/algorithmization_2/sort_of_array/Task01.java
@@ -9,34 +9,34 @@ public class Task01 {
 	 */
 
 	public static void main(String[] args) {		
-        int[] array1 = new int[]{1, 2, 3, 4, 8, 6, 4, 7};
-        int[] array2 = new int[]{10, 1, 11};
-        int k = 5;
-        int[] result = new int[array1.length + array2.length];    
-        
-        int j = 0;
-		for (int element : array1) {			
-			result[j] = element;
-			j++;		
-		}		
-		
-		for (int element : array2) {
-			result[j] = element;						
-			j++;			
-		}		
-		
-		int temp;
-		for (int i = 0; i < array2.length; i++) {
-			temp = i + k;
-			int temp2 = result[temp];
-			result[temp] = result[temp + array2.length];
-			result[temp + array2.length] = temp2;
-		}       
-        
-        System.out.println("Результирующий массив: ");        
-        for (int element : result) {
-            System.out.print("[" + element + "] ");
-        }
-        System.out.println();       
+		int[] array1 = new int[]{1, 2, 3, 4, 8, 6, 4, 7};
+		int[] array2 = new int[]{10, 1, 11};
+		int k = 5;
+		int[] result = new int[array1.length + array2.length];    
+
+		int j = 0;
+			for (int element : array1) {			
+				result[j] = element;
+				j++;		
+			}		
+
+			for (int element : array2) {
+				result[j] = element;						
+				j++;			
+			}		
+
+			int temp;
+			for (int i = 0; i < array2.length; i++) {
+				temp = i + k;
+				int temp2 = result[temp];
+				result[temp] = result[temp + array2.length];
+				result[temp + array2.length] = temp2;
+			}       
+
+		System.out.println("Результирующий массив: ");        
+		for (int element : result) {
+		    System.out.print("[" + element + "] ");
+		}
+		System.out.println();       
     }	
 }
