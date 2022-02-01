64311bffaca692d2ce261de8ebf7c7865b99f190
Iryna Valovich <83275959+IrynaValovich@users.noreply.github.com>
2022-02-01 14:07:01 +0300

    Update Task04.java

--- 2_Algorithmization/src/by/htp/algorithmization_2/decomposition/Task04.java
+++ 2_Algorithmization/src/by/htp/algorithmization_2/decomposition/Task04.java
@@ -15,18 +15,18 @@ public class Task04 {
 		int n;
 		n = getNPointsFromUser("Введите количество точек на плоскости (n) >> ");
 		
-	    double arrayX[] = new double[n];
-	    double arrayY[] = new double[n];
-	    
-	    System.out.println("Вводите координаты точек парами по очереди. Сначала X первой точки, после Y первой точки. \n" +
-			    "Затем X второй точки, Y второй точки и т.д.");	     
-	    for (int i = 0; i < n; i++){
-	        arrayX[i] = enterCoordFromConsole("Введите координату Х вашей точки, а затем нажмите ввод >> ");
-	        arrayY[i] = enterCoordFromConsole("Введите координату Y вашей точки, а затем нажмите ввод >> ");
-	    }
-	    
-	    printCoordinatesOfPoints(arrayX, arrayY);	        
-	    findMaxDistanceBetweenPoints(arrayX, arrayY);		
+		double arrayX[] = new double[n];
+		double arrayY[] = new double[n];
+
+		System.out.println("Вводите координаты точек парами по очереди. Сначала X первой точки, после Y первой точки. \n" +				   
+				    "Затем X второй точки, Y второй точки и т.д.");	     
+		for (int i = 0; i < n; i++) {			
+			arrayX[i] = enterCoordFromConsole("Введите координату Х вашей точки, а затем нажмите ввод >> ");
+			arrayY[i] = enterCoordFromConsole("Введите координату Y вашей точки, а затем нажмите ввод >> ");
+		}
+		
+		printCoordinatesOfPoints(arrayX, arrayY);
+		findMaxDistanceBetweenPoints(arrayX, arrayY);		
 	}
 	
 	private static int enterFromConsole(String message) {				
@@ -40,7 +40,7 @@ public class Task04 {
 	
 	private static int getNPointsFromUser(String message) {
 		int points;
-		do {
+		do {			
 			points = enterFromConsole(message);			
 		} while (points <= 1);			
 		return points;
@@ -58,26 +58,26 @@ public class Task04 {
 	private static void findMaxDistanceBetweenPoints(double x[], double y[]) {		
 		double distance;				
 		double distanceMax = 0;	   
-	    int indexFirstMaxPoint = 0;
-	    int indexSecondMaxPoint = 0;	    
-	    for (int i = 0; i < x.length - 1; i++) {
-	    	for (int j = i + 1; j < x.length; j++) {	    		
-                distance = Math.sqrt(Math.pow(x[j] - x[i], 2) + Math.pow(y[j] - y[i], 2));
-                if (distanceMax < distance) {
-                    distanceMax = distance;
-                    indexFirstMaxPoint = i;
-                    indexSecondMaxPoint = j;
-                }
-            }
-        }
-        System.out.println("Между точкой " + indexFirstMaxPoint + " и точкой " + indexSecondMaxPoint);	    
-	    System.out.println("максимальное расстояние, равное " + distanceMax);	    
+	    	int indexFirstMaxPoint = 0;
+	    	int indexSecondMaxPoint = 0;
+		for (int i = 0; i < x.length - 1; i++) {			
+			for (int j = i + 1; j < x.length; j++) {
+				distance = Math.sqrt(Math.pow(x[j] - x[i], 2) + Math.pow(y[j] - y[i], 2));
+				if (distanceMax < distance) {
+					distanceMax = distance;
+			    		indexFirstMaxPoint = i;
+			    		indexSecondMaxPoint = j;
+				}
+		   	 }
+        	}
+        	System.out.println("Между точкой " + indexFirstMaxPoint + " и точкой " + indexSecondMaxPoint);	    
+	    	System.out.println("максимальное расстояние, равное " + distanceMax);	    
 	}
 	
 	private static void printCoordinatesOfPoints(double x[], double y[]) {
-		for (int pointIndex = 0; pointIndex < x.length; pointIndex++) {	    	   
-	        System.out.print("\nЗначение координат для " + pointIndex + " точки: \n Х = " + x[pointIndex] 
-	        		+"\tY = " + y[pointIndex] + "\n");
-	    }		
+		for (int pointIndex = 0; pointIndex < x.length; pointIndex++) {
+			System.out.print("\nЗначение координат для " + pointIndex + " точки: \n Х = " + x[pointIndex]
+					 +"\tY = " + y[pointIndex] + "\n");
+	    	}		
 	}
 }
