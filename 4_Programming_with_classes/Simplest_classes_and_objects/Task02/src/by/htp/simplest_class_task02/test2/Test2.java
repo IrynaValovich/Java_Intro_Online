ed584cfcfb8b89b307f2cd0295ea22e5b82daa54
IrynaValovich <irynavalovich@gmail.com>
2022-02-06 12:35:24 +0100

    Test2

---
+++ 4_Programming_with_classes/Simplest_classes_and_objects/Task02/src/by/htp/simplest_class_task02/test2/Test2.java
@@ -0,0 +1,5 @@
+package by.htp.simplest_class_task02.test2;
+
+public class Test2 {
+
+}
---
+++ 4_Programming_with_classes/Simplest_classes_and_objects/Task02/src/by/htp/simplest_class_task02/test2/Test2Demo.java
@@ -0,0 +1,10 @@
+package by.htp.simplest_class_task02.test2;
+
+public class Test2Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
